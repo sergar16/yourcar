1cc584f87bac4327d91c636eee7fb2911b12c377
Serhii Hokhkalenko <serhii.hokhkalenko@gmail.com>
2017-04-17 15:46:28 +0200

    yourcar-ui initial committ

---
+++ src/main/java/org/serhiihokhkalenko/yourcar/controller/PingPongController.java
@@ -0,0 +1,13 @@
+package org.serhiihokhkalenko.yourcar.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingPongController {
+
+    @RequestMapping("/ping")
+    public String greeting() {
+        return "pong";
+    }
+}
